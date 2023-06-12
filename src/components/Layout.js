14eb9f58c5ec6d326cdfd2b097eadfe9a4a16b54
hamyngn <hamyngn@gmail.com>
2023-06-12 16:58:57 +0200

    modify: logo

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -2,13 +2,14 @@ import * as React from 'react';
 import { Outlet, NavLink } from 'react-router-dom';
 import styles from '../assets/styles/Layout.module.css'
 import logo from '../assets/images/logo.png'
+import logoSmall from '../assets/images/logo-small.png'
 
 const Layout = () => {
     return (
         <>
         <header className={styles.flexRow}>
         <div className={styles.title}>
-            <img src={logo} alt="Logo" className={styles.logo}/>
+            <img src={logo} alt="Logo" srcset={`${logoSmall} 480w, ${logo} 1080w`} sizes="50vw" className={styles.logo}/>
             <h1 style={{color: '#4F5A26'}}>HRnet</h1>
         </div>
         <div className={styles.navContainer}>
